4db356372fe8eb1f3d3bd7dbdd70fb1ff05304da
Firdavsi <firdavsinurov326@gmail.com>
2022-09-21 01:02:27 +0300

    localization

--- connect_frontend/packages/site/lib/main.dart
+++ connect_frontend/packages/site/lib/main.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+import 'package:site/i18n/strings.g.dart';
 
-void main() => runApp(
-      const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: Center(
-            child: Text('Hello, world!'),
-          ),
-        ),
-      ),
+part 'main.g.dart';
+
+void main() {
+  // ignore: avoid-ignoring-return-values
+  WidgetsFlutterBinding.ensureInitialized();
+  // ignore: avoid-ignoring-return-values
+  LocaleSettings.useDeviceLocale();
+
+  runApp(TranslationProvider(child: const _MyApp()));
+}
+
+@swidget
+Widget __myApp(BuildContext context) => MaterialApp(
+      locale: TranslationProvider.of(context).flutterLocale,
+      supportedLocales: LocaleSettings.supportedLocales,
+      localizationsDelegates: GlobalMaterialLocalizations.delegates,
+      debugShowCheckedModeBanner: false,
+      home: const _MyHomePage(),
     );
+
+@swidget
+Widget __myHomePage(BuildContext context) {
+  final trl = Translations.of(context);
+
+  return Scaffold(
+    body: Center(
+      child: Text(trl.title),
+    ),
+  );
+}
---
+++ connect_frontend/packages/site/lib/main.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+class _MyApp extends StatelessWidget {
+  const _MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext _context) => __myApp(_context);
+}
+
+class _MyHomePage extends StatelessWidget {
+  const _MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext _context) => __myHomePage(_context);
+}
